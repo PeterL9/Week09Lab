0a8554d2d3dde6200a69c07698aec5125a0feea3
799768 <799768@SAIT225783IT.ACDM.DS.SAIT.CA>
2021-03-21 13:54:31 -0600

    Changed UserDB getAll and get methods, changed DBUtil class

--- src/java/dataaccess/UserDB.java
+++ src/java/dataaccess/UserDB.java
@@ -5,68 +5,32 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.EntityManager;
 import models.User;
 
 public class UserDB {
 
     public List<User> getAll() throws Exception {
-        List<User> users = new ArrayList<>();
-        ConnectionPool cp = ConnectionPool.getInstance();
-        Connection con = cp.getConnection();
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        
-        String sql = "SELECT email, active, first_name, last_name, password, role, role_name FROM user, role WHERE role = role_id";
-        
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
         try {
-            ps = con.prepareStatement(sql);
-            rs = ps.executeQuery();
-            while (rs.next()) {
-                String email = rs.getString(1);
-                boolean active = rs.getBoolean(2);
-                String firstName = rs.getString(3);
-                String lastName = rs.getString(4);
-                String password = rs.getString(5);
-                int roleID = rs.getInt(6);
-                String roleName = rs.getString(7);
-                User user = new User(email, active, firstName, lastName, password, roleID, roleName);
-                users.add(user);
-            }
+            List<User> users = em.createNamedQuery("User.findAll", User.class).getResultList();
+            return users;
         } finally {
-            DBUtil.closeResultSet(rs);
-            DBUtil.closePreparedStatement(ps);
-            cp.freeConnection(con);
+            em.close();
         }
-        return users;
     }
 
     public User get(String email) throws Exception {
-        User user = null;
-        ConnectionPool cp = ConnectionPool.getInstance();
-        Connection con = cp.getConnection();
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        String sql = "SELECT * FROM user WHERE email =?";
+        // Create EntityManager
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
         
         try {
-            ps = con.prepareStatement(sql);
-            ps.setString(1, email);
-            rs = ps.executeQuery();
-            if (rs.next()) {
-                boolean active = rs.getBoolean(2);
-                String firstName = rs.getString(3);
-                String lastName = rs.getString(4);
-                String password = rs.getString(5);
-                int roleID = rs.getInt(6);
-                user = new User(email, active, firstName, lastName, password, roleID);
-            }
+            // Find user by email
+            User user = em.find(User.class, email);
+            return user;
         } finally {
-            DBUtil.closeResultSet(rs);
-            DBUtil.closePreparedStatement(ps);
-            cp.freeConnection(con);
+            em.close();
         }
-        
-        return user;
     }
 
     public void insert(User user) throws Exception {
