9b635f2dfd0bebfc66892fcd6b5037bd8dd92113
821214 <821214@SAIT225178IT.ACDM.DS.SAIT.CA>
2021-03-21 18:55:38 -0600

    Updated update method in UserService Update method now can update a user successfully

--- src/java/services/UserService.java
+++ src/java/services/UserService.java
@@ -24,8 +24,14 @@ public class UserService {
     }
     
     public void update(String email, Boolean active, String firstName, String lastName, String password) throws Exception {
-        User user = new User(email, active, firstName, lastName, password);
+        //User user = new User(email, active, firstName, lastName, password);
         UserDB userDB = new UserDB();
+        User user = userDB.get(email);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setEmail(email);
+        user.setPassword(password);
+        //user.setRole(role);
         userDB.update(user);
     }
     
